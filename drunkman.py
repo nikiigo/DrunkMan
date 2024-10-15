e18f97c62a023252fe37167eee028004f462b011
nikiigo <igor.nikitin@btinternet.com>
2024-10-14 21:09:31 -0600

    Fix types and comments

--- drunkman.py
+++ drunkman.py
@@ -6,11 +6,11 @@ from fractions import Fraction
 logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
 
 n = 10000  # Number of runs
-fp = 1 / 3  # step toward fall probability
-pos = 2  # initial position (1 is in 1 step to fall)
+fp = 1 / 3  # probability od the step towards the fall
+pos = 2  # initial position (1 is in 1 step away from falling)
 max_steps = 1000  # Almost infinity
 
-faults = 0
+falls = 0
 for attempts in range(1, n+1):
     steps = 0
     cur_pos = pos
@@ -22,11 +22,11 @@ for attempts in range(1, n+1):
             cur_pos += 1
         # logging.debug(f'current position = {cur_pos}')
         if cur_pos == 0:
-            faults += 1
-            logging.debug(f'saves ratio = {1 - faults/attempts}, {faults}, {attempts}')
+            falls += 1
+            logging.debug(f'saves ratio = {1 - falls / attempts}, {falls}, {attempts}')
             logging.debug(f'steps required to fall = {steps}')
         elif steps == max_steps:
-            logging.debug(f'saves ratio = {1 - faults/attempts}, {faults}, {attempts}')
+            logging.debug(f'saves ratio = {1 - falls / attempts}, {falls}, {attempts}')
             logging.debug(f'steps completed = {steps}')
-print(f' experimental result: saves ratio = {1 - faults/attempts}.')
-print(f' ratio = {Fraction(1 - faults/attempts).limit_denominator()}.')
+print(f' experimental result: saves ratio = {1 - falls / attempts}.')
+print(f' ratio = {Fraction(1 - falls / attempts).limit_denominator()}.')
